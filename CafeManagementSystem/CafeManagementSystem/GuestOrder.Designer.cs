5ca2f50cdee454168ef15a643c19ca9ea6004a9b
Warrant TSAI <warrant1997@gmail.com>
2023-10-11 23:18:44 +1100

    feat: finish guest order form

--- CafeManagementSystem/CafeManagementSystem/GuestOrder.Designer.cs
+++ CafeManagementSystem/CafeManagementSystem/GuestOrder.Designer.cs
@@ -29,51 +29,52 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.maskedTextBox3 = new System.Windows.Forms.MaskedTextBox();
+            this.QuantityTb = new System.Windows.Forms.MaskedTextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.OrdersGV = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.LabelAmount = new System.Windows.Forms.Label();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
-            this.ItemGV = new System.Windows.Forms.DataGridView();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.ItemsGV = new System.Windows.Forms.DataGridView();
+            this.CatCb = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ItemGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGV)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ItemsGV)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.White;
-            this.panel1.Controls.Add(this.maskedTextBox3);
+            this.panel1.Controls.Add(this.QuantityTb);
             this.panel1.Controls.Add(this.button2);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.OrdersGV);
             this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.LabelAmount);
             this.panel1.Controls.Add(this.maskedTextBox1);
             this.panel1.Controls.Add(this.maskedTextBox2);
-            this.panel1.Controls.Add(this.ItemGV);
-            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.ItemsGV);
+            this.panel1.Controls.Add(this.CatCb);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(136, 40);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(600, 432);
             this.panel1.TabIndex = 0;
             // 
-            // maskedTextBox3
+            // QuantityTb
             // 
-            this.maskedTextBox3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maskedTextBox3.Location = new System.Drawing.Point(33, 156);
-            this.maskedTextBox3.Name = "maskedTextBox3";
-            this.maskedTextBox3.Size = new System.Drawing.Size(126, 22);
-            this.maskedTextBox3.TabIndex = 23;
-            this.maskedTextBox3.Text = " Quantity";
+            this.QuantityTb.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.QuantityTb.Location = new System.Drawing.Point(271, 206);
+            this.QuantityTb.Name = "QuantityTb";
+            this.QuantityTb.Size = new System.Drawing.Size(126, 22);
+            this.QuantityTb.TabIndex = 23;
+            this.QuantityTb.Text = " Quantity";
+            this.QuantityTb.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // button2
             // 
@@ -94,20 +95,22 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.DarkBlue;
-            this.label3.Location = new System.Drawing.Point(315, 204);
+            this.label3.Location = new System.Drawing.Point(319, 233);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(111, 24);
             this.label3.TabIndex = 21;
             this.label3.Text = "Your Order";
             this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
-            // dataGridView1
+            // OrdersGV
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(189, 236);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(363, 162);
-            this.dataGridView1.TabIndex = 20;
+            this.OrdersGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.OrdersGV.Location = new System.Drawing.Point(189, 260);
+            this.OrdersGV.Name = "OrdersGV";
+            this.OrdersGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.OrdersGV.Size = new System.Drawing.Size(363, 138);
+            this.OrdersGV.TabIndex = 20;
+            this.OrdersGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
             // 
             // button1
             // 
@@ -115,24 +118,24 @@
             this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(33, 186);
+            this.button1.Location = new System.Drawing.Point(403, 204);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 39);
+            this.button1.Size = new System.Drawing.Size(92, 24);
             this.button1.TabIndex = 19;
             this.button1.Text = "Add To Cart";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // label2
+            // LabelAmount
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
-            this.label2.Location = new System.Drawing.Point(448, 403);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(104, 18);
-            this.label2.TabIndex = 18;
-            this.label2.Text = "Order Amount";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.LabelAmount.AutoSize = true;
+            this.LabelAmount.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelAmount.ForeColor = System.Drawing.Color.DarkBlue;
+            this.LabelAmount.Location = new System.Drawing.Point(448, 403);
+            this.LabelAmount.Name = "LabelAmount";
+            this.LabelAmount.Size = new System.Drawing.Size(0, 18);
+            this.LabelAmount.TabIndex = 18;
+            this.LabelAmount.Click += new System.EventHandler(this.label2_Click);
             // 
             // maskedTextBox1
             // 
@@ -152,26 +155,29 @@
             this.maskedTextBox2.TabIndex = 16;
             this.maskedTextBox2.Text = " Order Number";
             // 
-            // ItemGV
+            // ItemsGV
             // 
-            this.ItemGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.ItemGV.Location = new System.Drawing.Point(189, 39);
-            this.ItemGV.Name = "ItemGV";
-            this.ItemGV.Size = new System.Drawing.Size(363, 162);
-            this.ItemGV.TabIndex = 14;
+            this.ItemsGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ItemsGV.Location = new System.Drawing.Point(189, 39);
+            this.ItemsGV.Name = "ItemsGV";
+            this.ItemsGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.ItemsGV.Size = new System.Drawing.Size(363, 162);
+            this.ItemsGV.TabIndex = 14;
+            this.ItemsGV.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ItemsGV_CellContentClick);
             // 
-            // comboBox1
+            // CatCb
             // 
-            this.comboBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.CatCb.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CatCb.FormattingEnabled = true;
+            this.CatCb.Items.AddRange(new object[] {
             "Food",
             "Beverage"});
-            this.comboBox1.Location = new System.Drawing.Point(33, 51);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(126, 26);
-            this.comboBox1.TabIndex = 13;
-            this.comboBox1.Text = "Category";
+            this.CatCb.Location = new System.Drawing.Point(33, 51);
+            this.CatCb.Name = "CatCb";
+            this.CatCb.Size = new System.Drawing.Size(126, 26);
+            this.CatCb.TabIndex = 13;
+            this.CatCb.Text = "Category";
+            this.CatCb.SelectedIndexChanged += new System.EventHandler(this.CatCb_SelectedIndexChanged);
             // 
             // label1
             // 
@@ -225,10 +231,11 @@
             this.Name = "GuestOrder";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ItemForm";
+            this.Load += new System.EventHandler(this.GuestOrder_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.ItemGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.OrdersGV)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ItemsGV)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -240,15 +247,15 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.DataGridView ItemGV;
+        private System.Windows.Forms.ComboBox CatCb;
+        private System.Windows.Forms.DataGridView ItemsGV;
         private System.Windows.Forms.MaskedTextBox maskedTextBox2;
-        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LabelAmount;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView OrdersGV;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.MaskedTextBox maskedTextBox3;
+        private System.Windows.Forms.MaskedTextBox QuantityTb;
     }
 }
--- CafeManagementSystem/CafeManagementSystem/GuestOrder.cs
+++ CafeManagementSystem/CafeManagementSystem/GuestOrder.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 using System.Windows.Forms;
 
 namespace CafeManagementSystem
@@ -15,6 +18,49 @@ namespace CafeManagementSystem
         public GuestOrder()
         {
             InitializeComponent();
+        }
+        SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=F:\OneDrive\文件\Cafedb.mdf;Integrated Security=True;Connect Timeout=30");
+        void populate()
+        {
+            Console.WriteLine("Getting db data");
+            Con.Open();
+            string query = "SELECT * FROM ItemTbl";
+            SqlDataAdapter sda = new SqlDataAdapter(query, Con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            // Verify if the dataset contains any data
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                // Printing the retrieved data in the grid view
+                ItemsGV.DataSource = ds.Tables[0];
+                Console.WriteLine("Data retrieved successfully.");
+            }
+            else
+            {
+                Console.WriteLine("No data found in the database.");
+            }
+            Con.Close();
+        }
+        void filterByCategory()
+        {
+            Console.WriteLine("Filtering DB data");
+            Con.Open();
+            string query = "SELECT * FROM ItemTbl WHERE ItemCat = '" + CatCb.SelectedItem.ToString() + "'";
+            SqlDataAdapter sda = new SqlDataAdapter(query, Con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            // Verify if the dataset contains any data
+            if (ds.Tables.Count > 0 && ds.Tables[0].Rows.Count > 0)
+            {
+                ItemsGV.DataSource = ds.Tables[0];
+            }
+            else
+            {
+                Console.WriteLine("No data found in the database.");
+            }
+            Con.Close();
         }
 
         private void label7_Click(object sender, EventArgs e)
@@ -47,6 +93,63 @@ namespace CafeManagementSystem
             this.Hide();
             Dashboard dashboard = new Dashboard();
             dashboard.Show();
+        }
+
+        int num, amount = 0;
+        int price, qty, total;
+        DataTable table = new DataTable();
+        int flag = 0;
+        int sum = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (QuantityTb.Text == "")
+            {
+                MessageBox.Show("What is the Quantity of item?");
+            }
+            else if (flag == 0)
+            {
+                MessageBox.Show("Select The Product To Be Ordered.");
+            }
+            else
+            {
+                num = num + 1;
+                total = price * Convert.ToInt32(QuantityTb.Text);
+                table.Rows.Add(num, itemName, cat, price, total);
+                OrdersGV.DataSource = table;
+                flag = 0;
+            }
+            amount = amount + total;
+            LabelAmount.Text = " AUD" + amount.ToString();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void ItemsGV_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            itemName = ItemsGV.SelectedRows[0].Cells[2].Value.ToString();
+            cat = ItemsGV.SelectedRows[0].Cells[3].Value.ToString();
+            price = Convert.ToInt32(ItemsGV.SelectedRows[0].Cells[4].Value.ToString());
+            flag = 1;
+        }
+
+        string item, cat, itemName;
+        private void GuestOrder_Load(object sender, EventArgs e)
+        {
+            populate();
+            table.Columns.Add("Num", typeof(int));
+            table.Columns.Add("Item", typeof(string));
+            table.Columns.Add("Category", typeof(string));
+            table.Columns.Add("UnitPrice", typeof(int));
+            table.Columns.Add("Total", typeof(int));
+            OrdersGV.DataSource = table;
+        }
+
+        private void CatCb_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filterByCategory();
         }
     }
 }
